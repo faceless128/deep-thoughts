794b42dfb85092d4dea2ffd3cfd24c42dadfa763
Kaijam Jeffers <faceless128@gmail.com>
2022-06-28 22:50:35 -0400

    single thought tweak

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
@@ -19,10 +19,14 @@ const SingleThought = props => {
     <div>
       <div className="card mb-3">
         <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
+          <Link
+            to={`/profile/${thought.username}`}
+            style={{ fontWeight: 700 }}
+            className="text-light"
+          >
             {thought.username}
-          </span>{' '}
-          thought on {thought.createdAt}
+          </Link>{' '}
+            thought on {thought.createdAt}
         </p>
         <div className="card-body">
           <p>{thought.thoughtText}</p>
